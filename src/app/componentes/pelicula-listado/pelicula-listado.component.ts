6681d1cdf2fc7e54978f0ef9acb722012c7fa6e9
Matias Gravante <mgravante@navent.com>
2021-10-05 20:12:32 -0300

    fix oeliculas listado

--- src/app/componentes/pelicula-listado/pelicula-listado.component.ts
+++ src/app/componentes/pelicula-listado/pelicula-listado.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Pelicula } from 'src/app/clases/pelicula';
+import { Pelicula, Tipo } from 'src/app/clases/pelicula';
+import { PeliculaService } from 'src/app/servicios/pelicula.service';
 
 @Component({
   selector: 'app-pelicula-listado',
@@ -7,11 +8,30 @@ import { Pelicula } from 'src/app/clases/pelicula';
   styleUrls: ['./pelicula-listado.component.scss']
 })
 export class PeliculaListadoComponent implements OnInit {
-  @Input() public arrayPeliculas: Pelicula[]
+  @Input() arrayPeliculas: Pelicula[] = [];
+  @Input() peliculasListado: boolean = true;
 
-  constructor() { }
+  constructor(private peliculaService: PeliculaService) { }
 
   ngOnInit(): void {
+    if (this.peliculasListado) {
+      this.peliculaService.getPeliculas().subscribe((peliculas: any) => {
+        for (let index = 0; index < peliculas.length; index++) {
+          const peli = peliculas[index];
+          let id: string = peli.payload.doc.id;
+          let nombre: string = peli.payload.doc.data().nombre;
+          let tipo: Tipo = peli.payload.doc.data().tipo;
+          let fechaDeEstreno: string = peli.payload.doc.data().fechaDeEstreno;
+          let cantidadDePublico: number = peli.payload.doc.data().cantidadDePublico;
+          let fotoDeLaPelicula: string = peli.payload.doc.data().fotoDeLaPelicula;
+          let actorId: string = peli.payload.doc.data().actorId;
+          let peliNueva = new Pelicula(nombre, tipo, fechaDeEstreno, cantidadDePublico, fotoDeLaPelicula, actorId);
+          peliNueva.id = id;
+          this.arrayPeliculas.push(peliNueva)
+        }
+      })
+    }
+
   }
 
   limpiar() {
